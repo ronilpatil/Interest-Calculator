81a5d80f1e9c728cc2c82bb0ccbbc5cf5660e9fe
ronilpatil <69072652+ronilpatil@users.noreply.github.com>
2020-08-02 13:10:29 +0530

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,8 +1,21 @@
 function calculate(){
+
             const p=document.getElementById("Principal").value;
             const r=document.getElementById("Rate").value;
             const t=document.getElementById("Time").value;
-            const a=p*Math.pow(1+(r/100),t)
-            var result=document.getElementById("Result")
+            const checked=document.querySelector('input[name="choice"]:checked').value
+            var a=null
+            const result=document.getElementById("Result")
+            if(p==""||r==""||t==""){
+                result.innerHTML="Complete all the fields!"
+                return
+            }
+            if(checked=="Simple"){
+            a=(p*r*t)/100
+            }
+            else if(checked=="Compound"){
+            a=p*Math.pow(1+(r/100),t)
+            }
+           
             result.innerHTML="Total Amount: "+a.toFixed(2)
             }
