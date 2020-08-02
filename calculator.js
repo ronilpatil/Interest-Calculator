49e5f567a36f83da2a6a963247262be3faf90d1f
ronilpatil <69072652+ronilpatil@users.noreply.github.com>
2020-08-02 13:51:47 +0530

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,21 +1,23 @@
 function calculate(){
 
-            const p=document.getElementById("Principal").value;
-            const r=document.getElementById("Rate").value;
-            const t=document.getElementById("Time").value;
-            const checked=document.querySelector('input[name="choice"]:checked').value
-            var a=null
-            const result=document.getElementById("Result")
-            if(p==""||r==""||t==""){
-                result.innerHTML="Complete all the fields!"
-                return
-            }
-            if(checked=="Simple"){
-            a=p+((p*r*t)/100)
-            }
-            else if(checked=="Compound"){
-            a=p*Math.pow(1+(r/100),t)
-            }
-           
-            result.innerHTML="Total Amount: "+a.toFixed(2)
-            }
+    const p=document.getElementById("Principal").value;
+    const r=document.getElementById("Rate").value;
+    const t=document.getElementById("Time").value;
+    const checked=document.querySelector('input[name="choice"]:checked').value
+    var a=null
+    const result=document.getElementById("Result")
+    if(p==""||r==""||t==""){
+        result.innerHTML="Complete all the fields!"
+        return
+    }
+    if(checked=="Simple"){
+    a=p*(1+(r*t)/100)
+    }
+    else if(checked=="Compound"){
+    a=p*Math.pow(1+(r/100),t)
+    }
+   
+    result.innerHTML="Total Amount: "+a.toFixed(2)
+    return
+
+    }
